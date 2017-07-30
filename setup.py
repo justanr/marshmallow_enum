3e5d340292df955086aa9810059768619292b25b
Alec Nikolas Reiter <alecreiter@gmail.com>
2017-07-29 21:09:19 -0400

    Bump to 1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ REQUIRES = ['marshmallow>=2.0.0'] + extra_requires
 if __name__ == '__main__':
     setup(
         name='marshmallow-enum',
-        version='1.2',
+        version='1.3',
         author='Alec Nikolas Reiter',
         author_email='alecreiter@gmail.com',
         description='Enum field for Marshmallow',
