8e1fcf3ae3679a58d071ff7be4a34e753ffd1722
Samuel Hoffstaetter <samuel@hoffstaetter.com>
2020-04-17 21:51:15 -0700

    Restore deprecated load_by and dump_by parameters for backward compatibility

--- marshmallow_enum/__init__.py
+++ marshmallow_enum/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
+import logging
 import sys
 import warnings
+from enum import Enum
 
 from marshmallow import ValidationError
 from marshmallow.fields import Field
@@ -15,17 +17,62 @@ else:
     string_types = (str, )
     text_type = str
 
+logger = logging.getLogger(__name__)
+
+
+class LoadDumpOptions(Enum):
+    ''' Deprecated: Use the by_value parameter instead '''
+    value = 1
+    name = 0
+
 
 class EnumField(Field):
+    VALUE = LoadDumpOptions.value  # deprecated
+    NAME = LoadDumpOptions.name  # deprecated
+
     default_error_messages = {
         'by_name': 'Invalid enum member {input}',
         'by_value': 'Invalid enum value {input}',
         'must_be_string': 'Enum name must be string'
     }
 
-    def __init__(self, enum_type, by_value=True, error="", *args, **kwargs):
-        self.enum = enum_type
-        self.by_value = by_value
+    def __init__(self, enum_type, by_value=None, load_by=None, dump_by=None,
+                 error="", *args, **kwargs):
+        '''
+        The `load_by` and `dump_by` parameters are deprecated. Use the `by_value` parameter instead.
+
+        '''
+
+        if by_value is None:
+            if load_by is not None:
+                if dump_by is not None and load_by != dump_by:
+                    raise ValueError(
+                        'Deprecated `load_by` parameter must not differ from `dump_by` parameter')
+                by_value = (load_by == LoadDumpOptions.value)
+            elif dump_by is not None:
+                by_value = (dump_by == LoadDumpOptions.value)
+            else:
+                by_value = True
+
+        if load_by is not None:
+            logging.warning(
+                'The `load_by` parameter is deprecated for '
+                'marshmallow_enum.EnumField.__init__(). '
+                'Use the `by_value` parameter instead')
+            load_by_value = (load_by == LoadDumpOptions.value)
+            if load_by_value != by_value:
+                raise ValueError(
+                    'Deprecated load_by_value parameter differs from by_value parameter')
+
+        if dump_by is not None:
+            logging.warning(
+                'The `dump_by` parameter is deprecated for '
+                'marshmallow_enum.EnumField.__init__(). '
+                'Use the `by_value` parameter instead')
+            dump_by_value = (dump_by == LoadDumpOptions.value)
+            if dump_by_value != by_value:
+                raise ValueError(
+                    'Deprecated dump_by_value parameter differs from by_value parameter')
 
         if error and any(old in error for old in ('name}', 'value}', 'choices}')):
             warnings.warn(
@@ -35,6 +82,8 @@ class EnumField(Field):
                 stacklevel=2
             )
 
+        self.enum = enum_type
+        self.by_value = by_value
         self.error = error
 
         super(EnumField, self).__init__(*args, **kwargs)
